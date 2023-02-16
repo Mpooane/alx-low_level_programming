49221f10acc22c68265afbe3a30bdf9688b7af20
Mpooane <kananelomajoe@gmail.com>
2023-02-16 14:37:11 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Push one or more patches onto the stack
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{  puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+}   
