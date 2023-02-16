3c786e7dce6b72774fbf48cc1b45b97f7f4982b2
Mpooane <kananelomajoe@gmail.com>
2023-02-16 15:00:49 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - list section sizes and total size of binary files
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+   printf("Size of a char: %lu byte(s)", size of(char));
+   printf("Size of int: %lu byte(s)", size of (int));
+   printf("Size of a long int: %lu byte(s)", size of (long int));
+   printf("Size of a long long int: %lu byte(s)", size of(long long int));
+   printf("Size of a float: %lu byte(s)", size of (float));
+   return (0);
+}   
