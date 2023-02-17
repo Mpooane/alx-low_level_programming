f25aa98888d7351ec9ad38c9c53e2c60f5dda7a0
Mpooane <kananelomajoe@gmail.com>
2023-02-17 14:43:33 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,26 @@
-#includeb<stdio.h>
-
+#include <stdio.h>
 /**
- *
  * main - main function
  *
- * Return: always 0
+ * Return: 0, if succesful
  */
-
 int main(void)
 {
-	int i;
+	/*
+	 * prints all single integers, with comma and space after
+	 * loops through 0-9, prints them,prints comma,print space,
+	 * use only putchar
+	 */
+	int x;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (x = 0 ; x < 10 ; x++)
 	{
-		putchar(i + '0');
-		if (i != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((x % 10) + '0');
+		if (x == 9)
+		/*continue printing comma and space except if x == 9*/
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
