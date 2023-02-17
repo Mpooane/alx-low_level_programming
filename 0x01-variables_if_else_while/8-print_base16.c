fb53c67c095df41fab8a772d94c31cfaa4bf89cf
Mpooane <kananelomajoe@gmail.com>
2023-02-17 14:23:33 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-
 /**
  * main - main function
- * Return: always 0
- * */
-
+ *
+ * Return: 0, if succesful
+ */
 int main(void)
 {
-	int i;
+	/*
+	 * prints the hexadecimal base16,using putchar only
+	 * loops through 9-0,prints them simulating char with arithemtics
+	 * loops through a-f, prints them
+	 */
+	int x;
+	char y;
 
-	for (i = 0 ; i < 10 ; i++)
-		putchar(i + '0');
-	for (j = 'a' ; j <= f ; j++)
-		putchar(j);
+	for (x = 0 ; x < 10 ; x++)
+		putchar((x % 10) + '0');
+
+	for (y = 'a' ; y <= 'f' ; y++)
+		putchar(y);
 	putchar('\n');
+
 	return (0);
 }
